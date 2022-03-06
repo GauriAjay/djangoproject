1a2fca8a3f2e3ed8e5b3f59106817029f88a39db
gauri <gauriajay18@gmail.com>
2022-03-06 12:48:12 +0530

    login

--- accounts/views.py
+++ accounts/views.py
@@ -2,10 +2,13 @@ from django.shortcuts import render,redirect
 from django.views import View
 from .models import *
 from .forms import *
+
+
 # Create your views here.
 class Index(View):
     def get(self,request):
-        contact=Contact.objects.all()
+        user=request.user
+        contact=Contact.objects.filter(user=user)
         context={'number':contact}
         return render(request,'index.html',context)
 class AddContact(View):
@@ -14,9 +17,12 @@ class AddContact(View):
         context={'form':form}
         return render(request,'add.html',context)
     def post(self,request):
+        user=request.user
         form=AddContactForm(request.POST)
         if form.is_valid():
-            form.save()
+            f=form.save(commit=False)
+            f.user=user
+            f.save()
         return redirect('index')
 class EditContact(View):
     def get(self,request,id):
@@ -29,4 +35,13 @@ class EditContact(View):
         form=AddContactForm(request.POST,instance=contact)
         if form.is_valid():
             form.save()
+        return redirect('index')
+class DeleteContact(View):
+    def get(self,request,id):
+        contact=Contact.objects.get(id=id)
+        context={'contact':contact}
+        return render(request,'delete.html',context)
+    def post(self,request,id):
+        contact=Contact.objects.get(id=id)
+        contact.delete()
         return redirect('index')
